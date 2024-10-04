3515ec387fb57ada98b5acb6f1d90d7cc326ede1
James Willson <jsdoublel@gmail.com>
2024-10-04 15:36:18 -0500

    in progress stuff

--- alg/alg.go
+++ alg/alg.go
@@ -9,6 +9,13 @@ import (
 	"camus/prep"
 )
 
+type DP struct {
+	DP        []uint         // score for each dp subproblem
+	Branches  [][2]int       // branch for each dp subproblem
+	Backtrace [][]int        // prev subproblems for each dp subproblem
+	TreeData  *prep.TreeData // preprocessed data for our tree
+}
+
 func CAMUS(tre *tree.Tree, geneTrees []*tree.Tree) ([][2]int, error) {
 	fmt.Fprint(os.Stderr, "beginning data preprocessing")
 	td, err := prep.Preprocess(tre, geneTrees)
@@ -17,43 +24,118 @@ func CAMUS(tre *tree.Tree, geneTrees []*tree.Tree) ([][2]int, error) {
 	}
 	fmt.Fprint(os.Stderr, "preprocessing finished, beginning CAMUS")
 	n := len(td.Tree.Nodes())
-	dp := make([]uint, n, n)      // score for each dp subproblem
-	branches := make([][2]int, n) // branch for each dp subproblem
-	backtrace := make([][]int, n) // prev subproblems for each dp subproblem
+	dp := &DP{DP: make([]uint, n, n), Branches: make([][2]int, n), Backtrace: make([][]int, n), TreeData: td}
 	td.Tree.PostOrder(func(cur, prev *tree.Node, e *tree.Edge) (keep bool) {
 		if !cur.Tip() { // default value is 0, so we don't need to code a base case
-			lID, rID := td.Children[cur.Id()][0].Id(), td.Children[cur.Id()][1].Id()
-			score, branch, back := score(cur, dp, td)
-			noEdgeScore := dp[lID] + dp[rID]
+			lID, rID := dp.TreeData.Children[cur.Id()][0].Id(), dp.TreeData.Children[cur.Id()][1].Id()
+			score, branch, back := dp.score(cur)
+			noEdgeScore := dp.DP[lID] + dp.DP[rID]
 			if score > noEdgeScore {
-				dp[cur.Id()] = score
-				branches[cur.Id()] = branch
-				backtrace[cur.Id()] = back
+				dp.DP[cur.Id()] = score
+				dp.Branches[cur.Id()] = branch
+				dp.Backtrace[cur.Id()] = back
 			} else {
-				dp[cur.Id()] = noEdgeScore
-				backtrace[cur.Id()] = []int{lID, rID}
+				dp.DP[cur.Id()] = noEdgeScore
+				dp.Backtrace[cur.Id()] = []int{lID, rID}
 			}
 		}
 		return true
 	})
-	result := traceback(branches, backtrace)
+	result := dp.traceback()
 	return result, nil
 }
 
-func score(alpha *tree.Node, dp []uint, td *prep.TreeData) (uint, [2]int, []int) {
-	// loop over all possible values of u for a given alpha
+/* calculates score for given top node v; returns score, best edge, and dp lookups*/
+func (dp *DP) score(v *tree.Node) (uint, [2]int, []int) {
+	// loop over all possible values of u for a given v
 	//		scoreU(u, ...) + dp lookup on path from v to u
-	return 0, [2]int{0, 0}, []int{}
+	scores := make([]uint, 0)
+	edges := make([][2]int, 0)
+	backtraces := make([][]int, 0)
+	if v != dp.TreeData.Root {
+		score, w, back := dp.scoreU(v, v, v)
+		scores = append(scores, score)
+		edges = append(edges, [2]int{v.Id(), w})
+		backtraces = append(backtraces, back)
+	}
+	PostOrderU(v, func(cur, otherSubtree *tree.Node) {
+		// do dp lookups along path between v -> u
+		score, w, back := dp.scoreU(cur, otherSubtree, v)
+		scores = append(scores, score)
+		edges = append(edges, [2]int{v.Id(), w})
+		backtraces = append(backtraces, back)
+	})
+	maxScore, maxIdx := 0, 0
+	for i, s := range scores {
+		if s >= uint(maxScore) {
+			maxScore = int(s)
+			maxIdx = i
+		}
+	}
+	return uint(maxScore), edges[maxIdx], backtraces[maxIdx]
 }
 
 /* score branch u -> w (for w in subtree under sub); returns score, best w */
-func scoreU(u, sub *tree.Node, qSet []*prep.Quartet, td *prep.TreeData) (uint, int) {
-	// solve topology for every quartet in the quartet set
-	//		loop over all leaves in subtree
-	//		score edge directly above leaf
-	return 0, 0
+func (dp *DP) scoreU(u, sub, v *tree.Node) (uint, int, []int) {
+	qSet := dp.TreeData.QuartetSets[v.Id()]
+	// iterate through possible w
+	scores := make([]int, 0)
+	backtraces := make([][]int, 0)
+	SubtreePreOrder(sub, func(cur *tree.Node) {
+
+	})
+	return 0, 0, []int{}
 }
 
-func traceback(branches [][2]int, backtrace [][]int) [][2]int {
+func PostOrderU(cur *tree.Node, f func(cur, otherSubtree *tree.Node, lookupTotal uint)) {
+	if !cur.Tip() {
+		children := make([]*tree.Node, 0)
+		for _, n := range cur.Neigh() {
+			if p, err := cur.Parent(); err != nil {
+				panic(err)
+			} else if n != p {
+				children = append(children, n)
+			}
+		}
+		if len(children) != 2 {
+			panic("tree is not binary")
+		}
+		subtreePostOrderHelper(children[0], children[1], 0, f)
+		subtreePostOrderHelper(children[1], children[0], 0, f)
+	} else {
+	}
+
+}
+
+func subtreePostOrderHelper(cur, otherSubtree *tree.Node, lookupTotal uint, f func(cur, otherSubtree *tree.Node, lookupTotal uint)) {
+	for _, n := range cur.Neigh() {
+		if p, err := cur.Parent(); err != nil {
+			panic(err)
+		} else if n != p {
+			subtreePostOrderHelper(n, otherSubtree, lookupTotal, f)
+		}
+	}
+	f(cur, otherSubtree)
+}
+
+func SubtreePreOrder(cur *tree.Node, f func(cur *tree.Node)) {
+	f(cur)
+	for _, n := range cur.Neigh() {
+		if p, err := cur.Parent(); err != nil {
+			panic(err)
+		} else if n != p {
+			SubtreePreOrder(n, f)
+		}
+	}
+}
+
+func scoreW() {
+}
+
+// func solveTopology(q *prep.Quartet, u, taxa *tree.Node, td *prep.TreeData) bool {
+// 	return false
+// }
+
+func (dp *DP) traceback() [][2]int {
 	return nil
 }
