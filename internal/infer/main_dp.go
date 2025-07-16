900c048f9060ef2463d09d0b8071a2d84d5bc603
James Willson <jsdoublel@gmail.com>
2025-07-16 15:22:08 -0500

    untested feature modification

--- internal/infer/main_dp.go
+++ internal/infer/main_dp.go
@@ -87,7 +87,7 @@ func (cdp *cycleDP) get(i, k int) (uint, *cycleTraceNode) {
 
 // Runs Infer algorithm -- returns preprocessed tree data struct, quartet count stats, list of branches.
 // Errors returned come from preprocessing (invalid inputs, etc.).
-func Infer(tre *tree.Tree, geneTrees []*tree.Tree, qMode pr.QMode) (*gr.TreeData, [][]gr.Branch, error) {
+func Infer(tre *tree.Tree, geneTrees []*tree.Tree, qMode pr.QuartetFilterOptions) (*gr.TreeData, [][]gr.Branch, error) {
 	log.Println("beginning data preprocessing")
 	td, err := pr.Preprocess(tre, geneTrees, qMode)
 	if err != nil {
